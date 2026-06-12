72d61e717a038cf56501f210276f150102f37844
Purvi garg <purvigarg350@gmail.com>
2026-06-12 18:11:33 +0530

    Time: 8 ms (49.68%) | Memory: 135.5 MB (35.43%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int>ans;
+       
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<pivot){
+            ans.push_back(nums[i]);
+            }
+        }
+        // sort(ans.begin(),ans.end(),greater<int>());
+       for(int i=0;i<nums.size();i++){ 
+        if(nums[i]==pivot){
+         ans.push_back(pivot);}}
+
+ vector<int>ans1;
+ for(int j=0;j<nums.size();j++){
+            if(nums[j]>pivot){
+               ans1.push_back(nums[j]); 
+            }
+        }
+        // sort(ans1.begin(),ans1.end());
+
+
+ans.insert(ans.end(),ans1.begin(),ans1.end());
+
+
+return ans;
+    }
+};
