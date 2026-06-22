e42ca3a465bb6388c1f439f751c280636d40489d
Purvi garg <purvigarg350@gmail.com>
2026-06-22 18:49:36 +0530

    Time: 263 ms (7.85%) | Memory: 403.9 MB (10.73%) - LeetSync

---
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string largestMerge(string word1, string word2) {
+        string merge="";
+       int i=0;
+       int j=0;
+       
+       while(i<word1.size() && j<word2.size()){
+   
+        if( word1.substr(i)>word2.substr(j)){
+     merge.push_back(word1[i]);
+        i++;}
+
+        else{merge.push_back(word2[j]);
+          j++;
+        
+
+    }}
+   while(i<word1.size()){
+     merge.push_back(word1[i]);
+        i++;
+   }
+   while(j<word2.size()){
+     merge.push_back(word2[j]);
+       j++;
+   } 
+
+    
+    
+    
+    
+return merge;
+    }
+};
