d4d8ccb2d5179f4b4757b7e86fadae4e4f4b058b
Purvi garg <purvigarg350@gmail.com>
2026-06-23 18:14:44 +0530

    Time: 123 ms (5.49%) | Memory: 24 MB (61.47%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -13,9 +13,3 @@ vector<int> nums;
       return sum;
     }
 };
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(left,right);
- */
